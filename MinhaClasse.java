b0b9c4fa454c6a4424c9ec3a4a94c61a557466bb
FabianaLLima <fabianallima@hotmail.com>
2024-06-03 10:36:19 -0300

    Exercícios Java

---
+++ MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+
+    public static void main (String [] args) {
+
+        System.out.print ("Olá turma, sejam bem vindos!" );
+    }
+}
+
+// void não retorna nada, a Main é a metodo da classe principal, System é uma classe do sistema
+//Arquivo java - todo arquivo java deve começar com a letra maiscula, se a palavra for composta a segunda palavra tb deve estar com letra maiscula
+//Nomenclatura da linguagem. A classe deve possuir o mesmo nome do arquivo.java
+//Toda variavél deve começar com letra maiscúla, se for composta a primeira letra da segunda palavra é maiscula seguindo o padrão camelCase
