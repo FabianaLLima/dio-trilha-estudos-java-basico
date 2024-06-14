fb36799ff0fb4c0a573840c70f428f52c9832ba7
FabianaLLima <fabianallima@hotmail.com>
2024-06-14 15:57:24 -0300

    Exemplos de estruturas de Repetição While, Do while

---
+++ src/EstruturasDeRepeticao/ExemploBreakContinue.java
@@ -0,0 +1,15 @@
+//Break: Interrompe o laço de repetição 
+//Continue interrompe somente a iteração atual.
+
+public class ExemploBreakContinue {
+	public static void main(String[] args) {
+	
+	    for(int numero = 1; numero <=5; numero ++){
+		    if(numero == 3)
+			   continue;
+		
+		System.out.println(numero);
+		
+	    }
+    }
+}
